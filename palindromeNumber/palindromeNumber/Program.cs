99bb2bc34bc4080a154e6ff47a04966214dbb702
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-21 08:49:57 -0300

    Add: palindrome number exercise

---
+++ palindromeNumber/palindromeNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿int x = -121;
+
+char[] reverse = x.ToString().ToCharArray();
+Array.Reverse(reverse);
+string reversed = new string(reverse);
+
+Console.WriteLine(reversed);
+Console.WriteLine(reversed == x.ToString());
